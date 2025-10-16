f899191a4190216c2f027b909373c0216e87a24b
Biven-Mwembo <96986425+Biven-Mwembo@users.noreply.github.com>
2025-10-15 23:03:36 -0800

    Fixed

--- FinSys/src/Admin/AdminLayout.jsx
+++ FinSys/src/Admin/AdminLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./AdminSidebar";
-import "./Admin/AdminLayout.css";
+import "./AdminLayout.css";
 
 const AdminLayout = () => {
   return (
